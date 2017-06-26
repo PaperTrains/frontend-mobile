6e655970ede1ec185e02a972c8538f278e9b6037
Marc Zijderveld <marczijderveld@planet.nl>
2017-06-27 00:20:23 +0200

    Upload

--- uploadWIndow.php
+++ uploadWIndow.php
@@ -4,20 +4,20 @@
   <meta charset="utf-8">
   <title>Login-Papertrain</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="./app/css/style.css">
+  <link rel="stylesheet" href="./app/css/uploadWindows.css">
 </head>
 <body>
     <div class="main-container">
-        <div class="login-header">
-            <img src="./app/images/Header.png">
+        <div class="polaroidContainer">
+            <div class="upload">
+                <img class="camera" src="./app/images/camera_ICOON.png">
+                <span class="upload-text">Voeg uw foto toe</span>
+                <input type="file"/>
+            </div>
+            <div id="thumb-output"></div>
         </div>
-        <div class="login-form">
-            <input type="text" name="Gebruikersnaam" value="Gebruikersnaam"><br>
-            <input type="text" name="Wachtwoord" value="Wachtwoord"><br> 
-        </div>    
-
-        <a href="">sfbsbh</a>
     </div>
-  <script src="./app/js/scripts.js"></script>
+    <script src="./app/js/lib/jquery-3.2.1.min.js"></script>
+  <script src="./app/js/imagePreview.js"></script>
 </body>
 </html>
