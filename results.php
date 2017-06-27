cfa47fc63332899214d242323748de8ab35b5662
TimRomeijn <tim28@live.nl>
2017-06-27 21:55:07 +0200

    Finished results scherm
    Finished results scherm

---
+++ results.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Results</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="./app/css/results.css">
+</head>
+<body>
+    <div class="main-container">
+    
+        <div class="results-message">
+            <img src="./app/images/Ns_Logo_NB.png" class="ns-logo-nb">
+            <div class="results-text">
+                <p>Bedankt voor je inzending.<br>
+                <br>
+                Kijk over een tijdje nog<br>
+                eens voor leuke reacties!</p>  
+            </div>
+        </div>      
+        
+    </div>
+  <script src="./app/js/scripts.js"></script>
+</body>
+</html>
